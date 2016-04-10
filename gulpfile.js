229a9a284e13692943c3040d84457a1289c2da78
Umair Khalid <umair719@gmail.com>
2016-04-10 15:07:29 -0500

    updated project to use nodemon

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,12 @@
 "use strict";
 
 var gulp = require('gulp');
-
 var jshint = require('gulp-jshint');
 var jscs = require('gulp-jscs');
+var nodemon = require('gulp-nodemon');
 
 var jsFiles = ['*.js', 'src/**/*.js'];
+
 
 gulp.task('style', function () {
     return gulp.src(jsFiles)
@@ -40,5 +41,22 @@ gulp.task('inject', function () {
         .pipe(inject(injectSrc, injectOptions))
         .pipe(gulp.dest('./src/views'));
 });
+
+gulp.task('serve', ['style', 'inject'], function () {
+    var options = {
+        script: 'app.js',
+        delayTime: 1,
+        env: {
+            'PORT': 5000
+        },
+        watch: jsFiles
+    };
+
+    return nodemon(options)
+        .on('restart', function (ev) {
+            console.log('Restarting...');
+        });
+});
+
 
 
